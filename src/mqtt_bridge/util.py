5a6f2951a7425ba48431a9730f34e2b414885ecd
Christopher Mayershofer <christopher.mayershofer@gmx.de>
2020-11-07 13:26:11 +0100

    Fix python3 compatibility issues for json and msgpack deserialization

--- src/mqtt_bridge/util.py
+++ src/mqtt_bridge/util.py
@@ -12,24 +12,6 @@ def lookup_object(object_path, package='mqtt_bridge'):
     obj = getattr(module, obj_name)
     return obj
 
-""" 
-def monkey_patch_message_conversion():
-    """ modify _to_primitive_inst to distinct unicode and str conversion """
-    from rosbridge_library.internal.message_conversion import (
-        type_map, primitive_types, string_types, FieldTypeMismatchException,
-    )
-    def _to_primitive_inst(msg, rostype, roottype, stack):
-        # Typecheck the msg
-        msgtype = type(msg)
-        if msgtype in primitive_types and rostype in type_map[msgtype.__name__]:
-            return msg
-        elif msgtype is str and rostype in type_map[msgtype.__name__]:
-            return msg.decode("utf-8").encode("utf-8", "ignore")
-        raise FieldTypeMismatchException(roottype, stack, rostype, msgtype)
-    message_conversion._to_primitive_inst = _to_primitive_inst
-"""
-# Seems like Python3 can work without this patch.
-#monkey_patch_message_conversion()
 extract_values = message_conversion.extract_values
 populate_instance = message_conversion.populate_instance
 
