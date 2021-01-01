74b4fdb571874d1ea2c124b23bf355033bf84ce0
Junya Hayashi <junya.hayashi@groove-x.com>
2021-01-01 15:08:42 +0900

    add test for primitive data types

--- test/test_util.py
+++ test/test_util.py
@@ -1,7 +1,7 @@
 from mqtt_bridge.bridge import RosToMqttBridge
 from mqtt_bridge.util import extract_values, populate_instance, lookup_object
 from sensor_msgs.msg import Temperature
-from std_msgs.msg import Header
+from std_msgs.msg import Header, String, Bool
 
 
 def test_lookup_object():
@@ -20,9 +20,23 @@ def test_extract_values():
     assert expected == actual
 
 
+def test_extract_values_priimtive():
+    msg = String("hello")
+    expected = {'data': 'hello'}
+    actual = extract_values(msg)
+    assert expected == actual
+
+
 def test_populate_instance():
     msg_dict = {'header': {'stamp': {'secs': 0, 'nsecs': 0}, 'frame_id': '', 'seq': 0}, 'temperature': 25.2, 'variance': 0.0}
     msg = Temperature()
     populate_instance(msg_dict, msg)
     assert msg.temperature == 25.2
     assert msg.variance == 0.0
+
+
+def test_populate_instance_primitive():
+    msg_dict = {'data': True}
+    msg = Bool()
+    populate_instance(msg_dict, msg)
+    assert msg.data == True
