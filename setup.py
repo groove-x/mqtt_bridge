b62fe23f596adeb602b89a65ce0326dc09730819
kapilPython <kapildeshpande041193@gmail.com>
2021-10-10 03:13:47 -0700

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
-setup_args = generate_distutils_setup(
-    packages=['mqtt_bridge'],
-    package_dir={'': 'src'},
-    install_requires=['paho-mqtt', 'inject', 'msgpack-python']
-)
+package_name = 'mqtt_bridge'
 
-setup(**setup_args)
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kapil',
+    maintainer_email='kapildeshpande041193@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
